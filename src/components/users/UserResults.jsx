c3c2eaeb9d53c56c51a55be4a4b0697dfba7e63a
Dhanush V D Upadya <dhanushupadya@gmail.com>
2022-11-09 15:15:51 +0530

    Added Spinner Component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import React,{useEffect,useState} from 'react'
+import Spinner from '../layout/Spinner'
+
 function UserResults() {
   
     const [users,setUsers] = useState([])
@@ -19,13 +21,22 @@ function UserResults() {
      setUsers(data);
      setLoading(false);
     }
-    return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users.map((user)=>(
-            <h3>{user.login}</h3>
-      ))}
-    </div>
-  )
+
+    if(!loading){
+        return (
+        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+          {users.map((user)=>(
+                <h3>{user.login}</h3>
+          ))}
+        </div>
+      )
+        
+    }
+    else{
+        return(
+            <Spinner/>
+        )
+    }
 }
 
 export default UserResults
